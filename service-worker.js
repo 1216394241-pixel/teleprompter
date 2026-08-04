bae6ca9d849c9ad27c576db2724cfb368daa633b
1216394241-pixel <1216394241@qq.com>
2026-08-05 01:12:20 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='follow-prompter-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./transcription-worker.js','./manifest.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
