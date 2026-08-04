cb89512060b1c57e687ef6b9abd774ca4dc0a523
1216394241-pixel <1216394241@qq.com>
2026-08-05 01:41:18 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE='follow-prompter-v4';
+const CACHE='follow-prompter-v6';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./transcription-worker.js','./manifest.webmanifest'];
-self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
